332d3af0a20ddb61f5c54c269a9829d9160516c6
Mikha38 <oyks1379@yandex.ru>
2021-06-07 14:44:40 +0300

    work

--- src/components/Tunneling/Tunneling.js
+++ src/components/Tunneling/Tunneling.js
@@ -3,35 +3,35 @@ import css from './Tunneling.module.css'
 
 
 const Tunneling = (props) => {
-        let admin = true; //Начало админки
-        let dates = props.allDate.map(el => <Dates date={el} 
-                                                  getRingsByDate={props.getRingsByDate} 
-                                                  checkArrays={props.checkArrays} 
-                                                  admin={admin}
-                                                  dispatch={props.dispatch}
-        />)
-    return(
-          <div>
-            <table className={css.toptable}>
-              <thead>
-                <tr>
-                  <th>Проходка</th>
-                  <th colSpan="3">Монтаж</th>
-                  <th colSpan="2">Нагнетание</th>
-                </tr>
-                <tr>
-                  <th>№</th>
-                  <th>№</th>
-                  <th>A</th>
-                  <th>K</th>
-                  <th>№</th>
-                  <th>Объём</th>
-                </tr>
-              </thead>
-            </table>
-            {dates}
-          </div>
-    )
+  let admin = true; //Начало админки
+  let dates = props.allDate.map(el => <Dates    date={el} 
+                                                getRingsByDate={props.getRingsByDate} 
+                                                checkArrays={props.checkArrays} 
+                                                admin={admin}
+                                                dispatch={props.dispatch}
+                                                />)
+  return(
+        <div>
+          <table className={css.toptable}>
+            <thead>
+              <tr>
+                <th>Проходка</th>
+                <th colSpan="3">Монтаж</th>
+                <th colSpan="2">Нагнетание</th>
+              </tr>
+              <tr>
+                <th>№</th>
+                <th>№</th>
+                <th>A</th>
+                <th>K</th>
+                <th>№</th>
+                <th>Объём</th>
+              </tr>
+            </thead>
+          </table>
+          {dates}
+        </div>
+  )
 }
 
 export default Tunneling;
